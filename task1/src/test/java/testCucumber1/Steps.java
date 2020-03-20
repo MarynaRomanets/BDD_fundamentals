12a19180e709e2661f48418021952732a9f980aa
Maryna Romanets <marina.romanets.1@gmail.com>
2020-03-20 20:48:35 +0200

    first Cucumber

---
+++ task1/src/test/java/testCucumber1/Steps.java
@@ -0,0 +1,30 @@
+package testCucumber1;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Steps {
+    @When("users upload data on a project")
+    public void users_upload_data_on_a_project() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the server should handle it and return a success status")
+    public void the_server_should_handle_it_and_return_a_success_status() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("users want to get information on the Cucumber project")
+    public void users_want_to_get_information_on_the_Cucumber_project() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the requested data is returned")
+    public void the_requested_data_is_returned() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
